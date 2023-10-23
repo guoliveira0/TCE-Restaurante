5b4ca03b188aedcebc9807e86b1dc29b97662f8a
Lorena Zuba <119495484+LorenaZuba@users.noreply.github.com>
2023-10-23 07:54:19 -0300

    Create base index.php

---
+++ index.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Restaurante</title>
+</head>
+<body>
+    <?php include '#';?>
+    <main>
+    
+        
+    </main>
+
+    <footer>
+        <p>&copy;2023 - Lorena Zuba, Luiz Gustavo e Mariana Oliveira</p>
+    </footer>
+</body>
+</html>
