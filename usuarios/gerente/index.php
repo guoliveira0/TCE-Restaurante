ed8e9f11add8f0ba9324ffa1b4c1e3b4d0d03836
Lorena Zuba <119495484+LorenaZuba@users.noreply.github.com>
2023-11-16 16:19:46 -0300

    atualizações index de gerente

--- usuarios/gerente/index.php
+++ usuarios/gerente/index.php
@@ -1,3 +1,8 @@
+<?php
+//require_once '../../classes/util.class.php';
+//Util::isGerente()
+
+?>
 <!DOCTYPE html>
 <html lang="pt-br">
 <head>
@@ -6,38 +11,45 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Restaurante</title>
 </head>
-
 <body>
-    <?php include './padrao/cabecalho.inc.php'; ?>
+    <?php include '../inc/cabecalho.inc.php';?>
 
     <main>
-        <?php
-        require_once 'classes/r.class.php';
+    <h1>Gerente</h1>
+    <ul>
+        <li><a href="cadastrarusuarios.php">Cadastro de Usuários</a></li>
+        <li><a href="editarusuarios.php">Edição de Usuários</a></li>
+        <li><a href="relatoriousuarios.php">Relatório Usuários</a></li>
+        <li><a href="cadastrarnoticias.php">Cadastro de Notícia</a></li>
+        
+    </ul>
+
+    <h2>Notícias</h2>
+    
+    <?php
+        require_once '../../classes/r.class.php';
+            
         
         R::setup(
             'mysql:host=127.0.0.1;dbname=sistemarestaurante',
             'root',
             ''
         );
-
-        /*$usuario = R::dispense('usuario');
-        $usuario->email = 'gerente2@email.com';
-        $usuario->nome = 'Gerente2';
-        $usuario->perfil = 'caixa';
-        $usuario->senha = md5(123 . '__');
-        R::store($usuario);
-        R::close();*/
-        $noticia = R::findOneForUpdate('noticia', 'data = (SELECT MAX(data) FROM noticia)');
-        $noticia2 = R::findOneForUpdate('noticia', 'ORDER BY data DESC LIMIT 1 OFFSET 1');
-        echo "$noticia";
-        echo "$noticia2";
-     
-
-
+        // Criando uma lista de noticias de acordo cm o bd
+        $noticias = R::findAll('noticias', ' ORDER BY id DESC')
+        // Talvez # -> 'ORDER BY id DESC LIMIT 3' funcione
         ?>
+
+        <?php 
+        foreach ($noticias as $noticia) { ?>
+            <div class="noticia">
+                <p><?= substr($noticia->conteudo, 0, 100) . '...' ?></p>  <!-- Para aparecer '...' quando ultrapassa 100 caracteres !-->
+            </div>
+        <?php } ?>
+
+        <p><a href="todasnoticias.php">Mais notícias</a></p>
     </main>
 
-    <?php include './padrao/rodape.inc.php'; ?>
+    <?php include '../inc/rodape.inc.php';?>
 </body>
-
 </html>
