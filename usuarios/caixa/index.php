74c7cdd43a2de29d4fcc4bb3d2e5759c03433486
Lorena Zuba <119495484+LorenaZuba@users.noreply.github.com>
2023-11-22 11:01:10 -0300

    Update index.php

--- usuarios/caixa/index.php
+++ usuarios/caixa/index.php
@@ -7,17 +7,41 @@
     <title>Restaurante</title>
 </head>
 <body>
-    <?php include '../inc/cabecalho.inc.php';?>
+    <?php include '../../padrao/cabecalho.inc.php';?>
+
 
     <main>
     <h1>Caixa</h1>
     <ul>
-        <li><a href=""></a></li>
+        <li><a href="">COisa</a></li>
     </ul>
-    
-        
+
+    <?php
+    require_once '../../classes/r.class.php';
+            
+    R::setup(
+        'mysql:host=127.0.0.1;dbname=sistemarestaurante',
+        'root',
+        ''
+    );
+    // Criando uma lista de noticias de acordo cm o bd
+    $noticias = R::findAll('noticia', ' ORDER BY id DESC')
+    // Talvez # -> 'ORDER BY id DESC LIMIT 3' funcione
+    ?>
+
+    <h2>Notícias</h2>
+    <?php 
+    foreach ($noticias as $noticia) { ?>
+        <div class="noticia">
+        <p><?= $noticia->conteudo ?></p>
+            <!-- <p> substr($noticia->conteudo, 0, 100) . '...' </p>  Não funcionou bem!-->
+        </div>
+    <?php } ?>
+
+    <p><a href="todasnoticias.php">Mais notícias</a></p>
+
     </main>
 
-    <?php include '../inc/rodape.inc.php';?>
+    <?php include '../../padrao/rodape.inc.php';?>
 </body>
 </html>
